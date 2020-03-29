47e3b2c80c86a917933b31e5aefd1f97100597d2
David Alexz <alexz.chimi@gmail.com>
2020-03-29 14:35:39 +0300

    finished vers

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-window.addEventListener("keydown", function(event) {
+function playSound(event) {
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 	if (!audio) return; //stops the function from running all togheter
-
+	audio.currentTime = 0; //setting the time position, rewind from the start
 	audio.play();
-});
+	key.classList.add('playing');
+}
+
+function removeTransition(event) {
+	if (event.propertyName !== 'transform') return; //skip if it's not a transofrm
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
